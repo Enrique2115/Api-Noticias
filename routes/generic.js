a9199da68da69a5036548d09bdffc521ea25f355
carlosSploit <arturo14212000@gmail.com>
2022-03-22 20:29:20 -0500

    a 22 03 2022

--- routes/generic.js
+++ routes/generic.js
@@ -3,6 +3,7 @@ const rooutes = express.Router();
 //######################### rooutes ###################################
 //listar
 rooutes.get("/", (req, res) => {
+  /// ola buenos dias
   res.send("La ruta esta funcionando correctamente");
 });
 
