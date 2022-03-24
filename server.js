1946c80ba9055e00a58da6f214e13c65a9c1cf1e
Luis2115 <lmorochofebres@gmail.com>
2022-03-23 23:09:56 -0500

    corrigiendo ruta

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const mysql = require("mysql");
 const mysqlconnet = require("express-myconnection");
 const ejs = require("ejs");
 const path = require("path");
-const cors = require("cors")
+const cors = require("cors");
 const verifyToken = require("./config/tockenizer/tokenizer");
 /// rotas de app ---------------------------------
 const tokeniser = require("./config/tockenizer/router/routertoken");
 const imageftp = require("./config/serviceftp/uploudimage");
-const gimageftp = require("./config/serviceftp/getimage")
+const gimageftp = require("./config/serviceftp/getimage");
 const generico = require("./routes/generic");
 const participantes = require("./routes/participantes");
 const votaciones = require("./routes/votaciones");
@@ -19,7 +19,7 @@ const config = require("./config/config.json");
 //config
 const app = express();
 // si se desea utilizar mysql desabilita esto
-const dbopccion = config.bd.mysql
+const dbopccion = config.bd.mysql;
 
 app.set("port", process.env.PORT || config.apires.portpru);
 app.set("views", path.join(__dirname, "views"));
@@ -27,12 +27,12 @@ app.set("view engine", "ejs");
 
 //mydellwares ------------------------------------------------------------------
 // si se desea utilizar mysql desabilita esto
-app.use(mysqlconnet(mysql, dbopccion, 'single'))
+app.use(mysqlconnet(mysql, dbopccion, "single"));
 app.use(express.json());
 //app.use()
 
 // - Configuration origen de acceso de la api rest
-app.use(cors())
+app.use(cors());
 
 //rootas -----------------------------------------------------------------------
 //**** roota principal o gemerica */
@@ -44,9 +44,9 @@ app.use("/tokeniser", tokeniser);
 app.use("/genetic", verifyToken, generico);
 app.use("/partic", verifyToken, participantes);
 app.use("/votacion", verifyToken, votaciones);
-app.use("/ftp",verifyToken, imageftp)
-app.use("/gftp", gimageftp)
+app.use("/ftp", verifyToken, imageftp);
+app.use("/gftp", gimageftp);
 //resever runnig----------------------------------------------------------------
-app.listen(app.get("port"), config.apires.hosturl, () => {
+app.listen(app.get("port"), () => {
   console.log("servidor se encuentra corriendo por el puerto", app.get("port"));
 });
