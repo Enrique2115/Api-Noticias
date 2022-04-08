78b3af57dc71dc8c4617932683733b538ea4270d
carlosSploit <arturo14212000@gmail.com>
2022-04-08 13:04:57 -0500

    a 08 04 2022

--- models/md_votaciones.js
+++ models/md_votaciones.js
@@ -40,8 +40,15 @@ module.exports = class dbvotaciones {
         return (Array.isArray(results))?results:[];
     }
 
+    // extrae la puntuacion por participante y por votacion
     async puntos_x_participante(req, res,id_part,id_vot){
         let results = await conexibd.single_query(req, res,'SELECT COUNT(*) as puntaje FROM votaci_parti as v INNER JOIN participante p On p.id_negocio = v.id_parti WHERE v.id_votac = ? and p.id_negocio = ? group by puntaje, v.id_votac, p.id_negocio;',[id_vot,id_part])
+        return (Array.isArray(results))?results:[];
+    }
+
+    // extraer el puntaje por votacion
+    async puntos_x_votacion(req, res,id_vot){
+        let results = await conexibd.single_query(req, res,'SELECT COUNT(*) as puntaje FROM votaciones as v inner join votaci_parti vp on vp.id_votac = v.id_votaciones WHERE v.id_votaciones = ? group by v.id_votaciones;',[id_vot])
         return (Array.isArray(results))?results:[];
     }
 
@@ -58,13 +65,14 @@ module.exports = class dbvotaciones {
 
         for (let item of comproberes){
             let puntaje = await this.puntos_x_participante(req,res,item["id_negocio"],id);
-            
+            let puntajetot = await this.puntos_x_votacion(req,res,id);
             let resulte = (puntaje.length == 0)?0:puntaje[0].puntaje;
+            let resultetotal = (puntajetot.length == 0)?0:puntajetot[0].puntaje;
             // console.log(resulte);
             // console.log(`votaciones: ${id} - participantes : ${item["id_negocio"]}`)
             
             item["puntaje"] = resulte;
-            item["promedio"] = 0;
+            item["promedio"] = (resulte * 100) / resultetotal;
             // console.log(item);
             trasformdata.push(item);
         }
